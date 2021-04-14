467f3bbc987738bd545d2a2a9b349e4e27f8f229
MaresR0bert <robi_mares@yahoo.com>
2021-04-14 18:27:37 +0300

    added implicit answers of user

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -3,15 +3,15 @@ import Question from '../schemas/questionSchema.js'
 
 let router = express.Router();
 
-function mergeAnswersArrays(questions){
-    return questions.map(question =>{
+function mergeAnswersArrays(questions) {
+    return questions.map(question => {
         let answers = question.rightAnswers.concat(question.wrongAnswers);
         return {
-            "_id":question._id,
-            "questionBody":question.questionBody,
-            "answers":answers,
-            "difficulty":question.difficulty,
-            "username":question.username
+            "_id": question._id,
+            "questionBody": question.questionBody,
+            "answers": answers,
+            "difficulty": question.difficulty,
+            "username": question.username
         }
     })
 }
@@ -20,52 +20,56 @@ function arraysEqual(a, b) {
     if (a === b) return true;
     if (a == null || b == null) return false;
     if (a.length !== b.length) return false;
-  
+
     for (var i = 0; i < a.length; ++i) {
-      if (a[i] !== b[i]) return false;
+        if (a[i] !== b[i]) return false;
     }
     return true;
-  }
+}
 
-router.route('/implicitanswers/').get((req,res)=>{
-    Question.find().then(questions=> res.json(mergeAnswersArrays(questions))).catch(err => res.status(400).json('Err: '+err));
+router.route('/implicitanswersofuser/:name').get((req, res) => {
+    Question.find({ username: req.params.name }).then(questions => res.json(mergeAnswersArrays(questions))).catch(err => res.status(400).json('Err: ' + err));
 });
 
-router.route('/explicitanswersofuser/:name/').get((req,res)=>{
-    Question.find({username:req.params.name}).then(questions=> res.json(questions)).catch(err => res.status(400).json('Err: '+err));
+router.route('/implicitanswers/').get((req, res) => {
+    Question.find().then(questions => res.json(mergeAnswersArrays(questions))).catch(err => res.status(400).json('Err: ' + err));
 });
 
-router.route('/explicitanswers/').get((req,res)=>{
-    Question.find().then(questions=> res.json(questions)).catch(err => res.status(400).json('Err: '+err));
+router.route('/explicitanswersofuser/:name/').get((req, res) => {
+    Question.find({ username: req.params.name }).then(questions => res.json(questions)).catch(err => res.status(400).json('Err: ' + err));
+});
+
+router.route('/explicitanswers/').get((req, res) => {
+    Question.find().then(questions => res.json(questions)).catch(err => res.status(400).json('Err: ' + err));
 }); //no use
 
-router.route('/:id').get((req,res)=>{
-    Question.findById(req.params.id).then(questions=> res.json(questions)).catch(err => res.status(400).json('Err: '+err));
+router.route('/:id').get((req, res) => {
+    Question.findById(req.params.id).then(questions => res.json(questions)).catch(err => res.status(400).json('Err: ' + err));
 });
 
-router.route('/:id').delete((req,res)=>{
-    Question.findByIdAndDelete(req.params.id).then(()=>res.json('Question deleted')).catch(err => res.status(400).json('Err: '+err));
+router.route('/:id').delete((req, res) => {
+    Question.findByIdAndDelete(req.params.id).then(() => res.json('Question deleted')).catch(err => res.status(400).json('Err: ' + err));
 });
 
-router.route('/check/:id/').post((req,res)=>{
-    Question.findById(req.params.id).then(question=>{
-        if(arraysEqual(question.rightAnswers,req.body.answer)) res.json('Correct');
+router.route('/check/:id/').post((req, res) => {
+    Question.findById(req.params.id).then(question => {
+        if (arraysEqual(question.rightAnswers, req.body.answer)) res.json('Correct');
         else res.json('Wrong');
-    }).catch(err=>res.status(400).json('Err: '+err));
+    }).catch(err => res.status(400).json('Err: ' + err));
 })
 
-router.route('/add').post((req,res)=>{
+router.route('/add').post((req, res) => {
     const questionBody = req.body.questionBody;
     const rightAnswers = req.body.rightAnswers;
     const wrongAnswers = req.body.wrongAnswers;
     const difficulty = Number(req.body.difficulty);
     const username = req.body.username;
 
-    const newQuestion = new Question({questionBody,rightAnswers,wrongAnswers,difficulty,username});
-    newQuestion.save().then(()=>res.json('Question added')).catch(err=>res.status(400).json('Err: '+err));
+    const newQuestion = new Question({ questionBody, rightAnswers, wrongAnswers, difficulty, username });
+    newQuestion.save().then(() => res.json('Question added')).catch(err => res.status(400).json('Err: ' + err));
 });
 
-router.route('/update/:id').put((req,res)=>{
+router.route('/update/:id').put((req, res) => {
     Question.findById(req.params.id).then(question => {
         question.questionBody = req.body.questionBody;
         question.rightAnswers = req.body.rightAnswers;
@@ -73,8 +77,8 @@ router.route('/update/:id').put((req,res)=>{
         question.difficulty = Number(req.body.difficulty);
         question.username = req.body.username;
 
-        question.save().then(()=>res.json('Question updated')).catch(err=>res.status(400).json('Err: '+err));
-    }).catch(err=>res.status(400).json('Err: '+err));
+        question.save().then(() => res.json('Question updated')).catch(err => res.status(400).json('Err: ' + err));
+    }).catch(err => res.status(400).json('Err: ' + err));
 });
 
 export default router;
